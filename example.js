12f501c87ea38d6079892df6687edef150ec8576
BefidcOZ <befidc124019@outlook.com>
2026-07-30 17:04:50 +0800

    Update README and LICENSE,put all test files into .gitignore

--- example.js
+++ example.js
@@ -1,10 +1,10 @@
 import get_url from './api.js';
 
 const share_url = "https://wwbvc.lanzouv.com/b011m9azlg"; // Cloud
-const SoftwareTag = 'arm64-v8a'; // Local
+const SoftwareTag = 'arm64'; // Local
 const pwd = "dtu2"; // Cloud
-const version_tag = ''; // Cloud
-const target_format = 'app-{SoftwareTag}-release.apk'; // Cloud
+const version_tag = '1.3.0-30-20260729'; // Cloud
+const target_format = 'PixelPlay-{version_tag}-{SoftwareTag}-release.apk'; // Cloud
 //EXAMPLES FOR TESTING
 
 const url = await get_url(version_tag, SoftwareTag, target_format, pwd, share_url);
